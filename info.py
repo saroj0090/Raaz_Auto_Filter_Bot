58de11b2c8d8914cf5310a99447d0cacde9621de
saroj0090 <Sarojkumar95566@gmail.com>
2025-12-10 14:40:14 +0530

    Update BOT_TOKEN in info.py
    Replaced placeholder BOT_TOKEN with actual token.

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'dreamxbotz_search')   # Session name for the bot
 API_ID = int(environ.get('API_ID', '')) # API ID from my.telegram.org
 API_HASH = environ.get('API_HASH', '')  # API Hash from my.telegram.org
-BOT_TOKEN = environ.get('BOT_TOKEN', "")    # Bot token from @BotFather
+BOT_TOKEN = environ.get('BOT_TOKEN', "7862229930:AAFSKlzIILYa9KSRRj_VPioEAKKKPWS78cU")    # Bot token from @BotFather
 
 # ============================
 # Bot Settings Configuration
